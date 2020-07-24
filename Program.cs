ae2a381471111a1743e48003ebd17faca233670c
alex w <103083020@student.swin.edu.au>
2020-07-24 15:22:42 +1000

    clean and add note

--- Program.cs
+++ Program.cs
@@ -8,37 +8,27 @@ namespace Problem
     {
         static void Main(string[] args)
         {
-            int ABCD = 0000;
-
-            for (int i = 0; i < 9999; i++)
+            int ABCD = 0;
+            for (int i = 0; i < 5000; i++)  
             {
                 int A = ABCD / 10 / 10 / 10 % 10;
                 int B = ABCD / 10 / 10 % 10;
                 int C = ABCD / 10 % 10;
                 int D = ABCD % 10;
-                string DC = D + "" + C + "" + B + "" + A;
-                int DCBA = int.Parse(DC);
-
-                if (A < 5)
+                int DCBA = int.Parse(D + "" + C + "" + B + "" + A);
+                
+                if ((A != B && A != C && A != D && B != C && B != D && C != D))
                 {
-                    if ((A != B && A != C && A != D && B != C && B != D && C != D))
+                    for (int E = 2; E < 9; E++)
                     {
-                        for (int E = 1; E < 9; E++)
+                        if (ABCD * E == DCBA)
                         {
-                            if (ABCD * E == DCBA)
-                            {
-                                System.Console.WriteLine(ABCD  + " * " + E + " = " + DCBA );
-                            }
+                            System.Console.WriteLine(ABCD + " * " + E + " = " + DCBA);
                         }
                     }
                 }
-                else
-                {
-                    // Blist.Add(i);
-                }
                 ABCD++;
             }
-
         }
     }
 }
